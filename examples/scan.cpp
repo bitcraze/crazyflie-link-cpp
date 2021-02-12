239c9f52c2bdc4f07b5542de18fc409c1737809f
Arnaud Taffanel <arnaud@bitcraze.se>
2021-02-12 11:20:02 +0100

    Fix bug in scann.cpp

--- examples/scan.cpp
+++ examples/scan.cpp
@@ -8,7 +8,7 @@ int main()
     // scanning
     const auto uris = Connection::scan();
 
-    for (const auto uri : uris) {
+    for (const auto& uri : uris) {
         std::cout << uri << std::endl;
     }
 
