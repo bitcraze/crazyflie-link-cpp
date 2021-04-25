e6af2f66bbfbfd2e0048b2803db84eeed8aab539
Steven Sorokin <67019760+Steven-Sorokin@users.noreply.github.com>
2021-04-25 14:38:48 +0300

    output the number of params

--- examples/benchmark.cpp
+++ examples/benchmark.cpp
@@ -33,93 +33,32 @@ public:
         }
 
         Packet p;
-        p.setPort(15);   // Link port
-        p.setChannel(1); // echo channel
+        // p.setPort(15);   // Link port
+        p.setPort(2);   // param port
+        p.setChannel(0); // toc channel
         p.setPayloadSize(sizeof(uint32_t));
-        
-        // Latency experiment
-        double latency_sum = 0.0;
-        latency_min = std::numeric_limits<double>::max();
-        const size_t count = 1000;
-        for (uint32_t i = 0; i < count; ++i)
-        {
-            std::memcpy(p.payload(), &i, sizeof(i));
-            auto start = std::chrono::steady_clock::now();
-            con.send(p);
-            Packet p_recv = con.recv(0);
-            auto end = std::chrono::steady_clock::now();
-            std::chrono::duration<double> diff = end - start;
+        int i = 3;
+        std::memcpy(p.payload(), &i, sizeof(i));
+        con.send(p);
+        Packet p_recv = con.recv(0);
 
-            uint32_t recv_i;
-            std::memcpy(&recv_i, p_recv.payload(), sizeof(recv_i));
-            if (p_recv.payloadSize() != p.payloadSize() || i != recv_i)
-            {
-                std::stringstream sstr;
-                sstr << "Latency: Sent " << i << " but received " << recv_i << "! (" << p_recv << ")";
-                throw std::runtime_error(sstr.str());
-            }
-            latency_sum += diff.count();
-            latency_min = std::min(diff.count(), latency_min);
-            // std::cout << p_recv << " " << diff.count() << std::endl;
-        }
-        latency_avg = latency_sum / (double)count;
-
-        // Bandwidth experiment
-        auto start = std::chrono::steady_clock::now();
-        for (uint32_t i = 0; i < count; ++i)
-        {
-            std::memcpy(p.payload(), &i, sizeof(i));
-            con.send(p);
-        }
-        Packet p_old;
-        for (uint32_t i = 0; i < count; ++i)
-        {
-            Packet p_recv = con.recv(0);
-            uint32_t recv_i;
-            std::memcpy(&recv_i, p_recv.payload(), sizeof(recv_i));
-            if (p_recv.payloadSize() != p.payloadSize() || i != recv_i)
-            {
-                std::stringstream sstr;
-                sstr << "Bandwith: Sent " << i << " but received " << recv_i << "! (" << p_recv << ")";
-
-                // DBG
-                sstr << std::endl;
-                sstr << " previous: " << p_old << std::endl;
-                sstr << " current: " << p_recv << std::endl;
-                sstr << " new/future ";
-                for (size_t k = 0; k < 10; ++k) {
-                    Packet p_recv2 = con.recv(0);
-                    sstr << p_recv2 << std::endl;
-                }
-
-                // DBG
-
-
-                throw std::runtime_error(sstr.str());
-            }
-            p_old = p_recv;
-        }
-        auto end = std::chrono::steady_clock::now();
-        std::chrono::duration<double> diff = end - start;
-        double total_duration = diff.count();
-        bandwidth = count / total_duration;
+        std::cout << p_recv << std::endl;
+        int num_param = p_recv.payload()[1];
+        std::cout << num_param << std::endl;
     }
 
 
 public:
-    double latency_avg;
-    double latency_min;
-    double bandwidth;
-
+    
 private:
     std::string uri_;
-
 };
+
 
 int main()
 {
-    // std::vector<std::string> uris({"radio://*/80/2M/E7E7E7E7E7", "radio://*/90/2M/E7E7E7E7E7"});
     std::vector<std::string> uris({"usb://0"});
+    std::cout << "connecting to crazyflie via usb 0" << std::endl;
 
     std::vector<Benchmark> benchmarks;
     benchmarks.reserve(uris.size());
@@ -135,10 +74,10 @@ int main()
         t.join();
     }
 
-    for (auto& b: benchmarks) {
-        std::cout << "Latency: Min: " << b.latency_min * 1000.0f << " ms; Avg: " << b.latency_avg * 1000.0f << " ms" << std::endl;
-        std::cout << "Bandwidth: " << b.bandwidth << " packets/s" << std::endl;
-    }
+    // for (auto& b: benchmarks) {
+    //     std::cout << "Latency: Min: " << b.latency_min * 1.0f << " ms; Avg: " << b.latency_avg * 1000.0f << " ms" << std::endl;
+    //     std::cout << "Bandwidth: " << b.bandwidth << " packets/s" << std::endl;
+    // }
 
     return 0;
 }
