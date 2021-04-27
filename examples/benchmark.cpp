52ad75b3a718901a5988e0f91739560af02e93a8
trakevital <32649570+trakevital@users.noreply.github.com>
2021-04-27 14:44:14 +0300

    Corrected the code according to new protocol
    Was able to print the crc and number of elements of the TOC

--- examples/benchmark.cpp
+++ examples/benchmark.cpp
@@ -8,12 +8,20 @@
 
 #include "crazyflieLinkCpp/Connection.h"
 
+#define PARAM_PORT 2
+#define TOC_CHANNEL 0
+
+#define CMD_TOC_ELEMENT 0 // original version: up to 255 entries
+#define CMD_TOC_INFO 1    // original version: up to 255 entries
+#define CMD_TOC_ITEM_V2 2 // version 2: up to 16k entries
+#define CMD_TOC_INFO_V2 3 // version 2: up to 16k entries
+
 using namespace bitcraze::crazyflieLinkCpp;
 
 class Benchmark
 {
 public:
-    Benchmark(const std::string& uri)
+    Benchmark(const std::string &uri)
         : uri_(uri)
     {
     }
@@ -32,63 +40,68 @@ public:
             }
         }
 
-        sendInt(2,0,0, con);
+        //ask for info
+        sendInt(con, PARAM_PORT, TOC_CHANNEL, CMD_TOC_INFO_V2);
 
-        sendInt(2,0,3, con);
-        
         Packet p_recv = con.recv(0);
         std::cout << p_recv << std::endl;
-        int num_param = p_recv.payload()[1];
-        std::cout << num_param << std::endl;
+        uint16_t num_of_elements = 0;
+        uint32_t crc = 0;
+        memcpy(&num_of_elements, &p_recv.payload()[1], sizeof(num_of_elements));
+        memcpy(&crc, &p_recv.payload()[3], sizeof(crc));
+        std::cout <<"num of elements: "<< num_of_elements << std::endl;
+        std::cout << "crc: "<<crc << std::endl;
+        // p_recv = con.recv(0);
+        // sendInt(2,0,0, con);
+        // std::cout << p_recv << std::endl;
+        // do
+        // {
+        //     p_recv = con.recv(0);
+        // } while (p_recv.port() != 2);
+        // std::cout << p_recv << std::endl;
 
-        p_recv = con.recv(0);
-        sendInt(2,0,0, con);
-        std::cout << p_recv << std::endl;
-        do
-        {
-            p_recv = con.recv(0);
-        } while (p_recv.port() != 2);
-        std::cout << p_recv << std::endl;
-    
+        // sendInt(2,0,1, con);
+        // do
+        // {
+        //     p_recv = con.recv(0);
+        // std::cout << p_recv << std::endl;
 
-        sendInt(2,0,1, con);
-        do
-        {
-            p_recv = con.recv(0);
-        std::cout << p_recv << std::endl;
+        // } while (p_recv.port() != 2);
 
-        } while (p_recv.port() != 2);
+        // sendInt(2,0,1, con);
+        // do
+        // {
+        //     p_recv = con.recv(0);
+        //     if(p_recv.port()==0)
+        //     std::cout << p_recv.payload() << std::endl;
 
-        sendInt(2,0,1, con);
-        do
-        {
-            p_recv = con.recv(0);
-            if(p_recv.port()==0)
-            std::cout << p_recv.payload() << std::endl;
-
-        } while (p_recv.port() != 2);
-        
+        // } while (p_recv.port() != 2);
     }
-        
 
-    void sendInt(int port, int channel, int intigerToSend, Connection& con)
+    void sendInt(Connection &con, int port, int channel, int intigerToSend, int extraData = 0)
     {
         Packet p;
-        p.setPort(port);   
-        p.setChannel(channel); 
-        p.setPayloadSize(sizeof(uint32_t));
-        std::memcpy(p.payload(), &intigerToSend, sizeof(intigerToSend));
+        p.setPort(port);
+        p.setChannel(channel);
+        if (extraData != 0)
+        {
+            p.setPayloadSize(sizeof(uint32_t) * 2);
+            std::memcpy(p.payload(), &intigerToSend, sizeof(intigerToSend));
+            std::memcpy(p.payload() + sizeof(uint32_t), &extraData, sizeof(extraData));
+        }
+        else
+        {
+            p.setPayloadSize(sizeof(uint32_t));
+            std::memcpy(p.payload(), &intigerToSend, sizeof(intigerToSend));
+        }
 
         con.send(p);
     }
 
-
 public:
-    
 private:
     std::string uri_;
 };
-
 
 int main()
 {
@@ -100,12 +113,14 @@ int main()
     std::vector<std::thread> threads;
     threads.reserve(uris.size());
 
-    for (const auto& uri : uris) {
+    for (const auto &uri : uris)
+    {
         benchmarks.emplace_back(Benchmark(uri));
         threads.emplace_back(std::thread(&Benchmark::run, &benchmarks.back()));
     }
 
-    for (auto& t : threads) {
+    for (auto &t : threads)
+    {
         t.join();
     }
 
