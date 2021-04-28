0cb3226906db0d3fa0e68fe9a3e1d5211eb4ba40
noamco3 <noamco67@gmail.com>
2021-04-28 14:22:47 +0300

    Corrected Toc.h and .cpp, with implementation

--- src/Toc.cpp
+++ src/Toc.cpp
@@ -1,183 +1,92 @@
-#include <iostream>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <chrono>
-#include <sstream>
-#include <queue>
-#include <vector>
+#include "Toc.h"
 
-#include "crazyflieLinkCpp/Connection.h"
 
-#define PARAM_PORT 2
-#define TOC_CHANNEL 0
 
-#define CMD_TOC_ELEMENT 0 // original version: up to 255 entries
-#define CMD_TOC_INFO 1    // original version: up to 255 entries
-#define CMD_TOC_ITEM_V2 2 // version 2: up to 16k entries
-#define CMD_TOC_INFO_V2 3 // version 2: up to 16k entries
-
-using namespace bitcraze::crazyflieLinkCpp;
-
-struct TocItem
+TocItem::TocItem(bitcraze::crazyflieLinkCpp::Packet &p_recv)
 {
-    std::string _groupName;
-    std::string _paramName;
-    uint8_t _cmdNum;
-    uint8_t _paramType;
-    uint16_t _paramId;
+    _cmdNum = p_recv.payload()[0];
+    _paramId = 0;
+    memcpy(&_paramId, &p_recv.payload()[1], sizeof(_paramId));
+    _paramType = p_recv.payload()[3];
+    _groupName = (char *)(&p_recv.payload()[4]);
+    _paramName = (char *)(&p_recv.payload()[4] + _groupName.length() + 1);
+}
 
-    TocItem(Packet &p_recv)
-    {
-        _cmdNum = p_recv.payload()[0];
-        _paramId = 0;
-        memcpy(&_paramId, &p_recv.payload()[1], sizeof(_paramId));
-        _paramType = p_recv.payload()[3];
-        _groupName = (char *)(&p_recv.payload()[4]);
-        _paramName = (char *)(&p_recv.payload()[4] + _groupName.length() + 1);
-    }
-    friend std::ostream &operator<<(std::ostream &out, const TocItem &tocItem)
-    {
-        out << "cmdNum: " << (int)tocItem._cmdNum << std::endl;
-        out << "paramId: " << (int)tocItem._paramId << std::endl;
-        out << "paramType: " << (int)tocItem._paramType << std::endl;
-        out << "groupName: " << tocItem._groupName << std::endl;
-        out << "paramName: " << tocItem._paramName << std::endl;
-        return out;
-    }
-};
-
-struct TocInfo
+std::ostream &operator<<(std::ostream &out, const TocItem &tocItem)
 {
-    uint16_t _numberOfElements;
-    uint32_t _crc;
+    out << "cmdNum: " << (int)tocItem._cmdNum << std::endl;
+    out << "paramId: " << (int)tocItem._paramId << std::endl;
+    out << "paramType: " << (int)tocItem._paramType << std::endl;
+    out << "groupName: " << tocItem._groupName << std::endl;
+    out << "paramName: " << tocItem._paramName << std::endl;
+    return out;
+}
 
-    TocInfo(Packet& p_recv)
-    {
-        memcpy(&_numberOfElements, &p_recv.payload()[1], sizeof(_numberOfElements));
-        memcpy(&_crc, &p_recv.payload()[3], sizeof(_crc));
-    }
-
-    friend std::ostream &operator<<(std::ostream &out, const TocInfo &tocInfo)
-    {
-        out << "numberOfElements: " << (int)tocInfo._numberOfElements << std::endl;
-        out << "crc: " << (int)tocInfo._crc << std::endl;
-        return out;
-    }
-};
-
-class Benchmark
+TocInfo::TocInfo(bitcraze::crazyflieLinkCpp::Packet &p_recv)
 {
-public:
-    Benchmark(const std::string &uri)
-        : uri_(uri)
-    {
-    }
+    memcpy(&_numberOfElements, &p_recv.payload()[1], sizeof(_numberOfElements));
+    memcpy(&_crc, &p_recv.payload()[3], sizeof(_crc));
+}
 
-    void run()
-    {
-        Connection con(uri_);
+std::ostream &operator<<(std::ostream &out, const TocInfo &tocInfo)
+{
+    out << "numberOfElements: " << (int)tocInfo._numberOfElements << std::endl;
+    out << "crc: " << (int)tocInfo._crc << std::endl;
+    return out;
+}
 
-        // empty receiver queue
-        while (true)
+Toc::Toc(const std::string &uri)
+    : _con(uri)
+{
+}
+
+void Toc::run()
+{
+
+    // empty receiver queue
+    while (true)
+    {
+        bitcraze::crazyflieLinkCpp::Packet p = _con.recv(100);
+        if (!p)
         {
-            Packet p = con.recv(100);
-            if (!p)
-            {
-                break;
-            }
-        }
-        
-        printToc(con);
-      
-        
-    }
-    TocInfo getTocInfo(Connection& con)
-    {
-        //ask for the toc info
-        sendInt(con, PARAM_PORT, TOC_CHANNEL, CMD_TOC_INFO_V2);
-        Packet p_recv = con.recv(0);
-        return TocInfo(p_recv);
-    }
-    TocItem getItemFromToc(Connection& con, uint16_t id)
-    {
-        //ask for a param with the given id
-        sendInt(con, PARAM_PORT, TOC_CHANNEL, CMD_TOC_ITEM_V2, id);
-        Packet p_recv = con.recv(0);
-        return TocItem (p_recv);
-    }
-
-    std::vector<TocItem> getToc(Connection& con)
-    {
-        std::vector<TocItem> tocItems;
-
-        uint16_t num_of_elements = getTocInfo(con)._numberOfElements;
-        for (uint16_t i = 0; i < num_of_elements; i++)
-        {
-            tocItems.push_back(getItemFromToc(con, i));
-        }
-        return tocItems;
-    }
-    void printToc(Connection& con)
-    {
-        auto tocItems = getToc(con);
-        for(TocItem tocItem : tocItems)
-        {
-            // tocItem
-            std::cout << tocItem._paramId << ": " << (int)tocItem._paramType << "  " << tocItem._groupName << "." << tocItem._paramName << std::endl;
+            break;
         }
     }
 
-    void sendInt(Connection &con, int port, int channel, uint8_t intigerToSend)
-    {
-        Packet p;
-        p.setPort(port);
-        p.setChannel(channel);
-
-        p.setPayloadSize(sizeof(intigerToSend));
-        std::memcpy(p.payload(), &intigerToSend, sizeof(intigerToSend));
-
-        con.send(p);
-    }
-
-    void sendInt(Connection &con, int port, int channel, uint8_t intigerToSend, uint8_t extraData)
-    {
-        Packet p;
-        p.setPort(port);
-        p.setChannel(channel);
-
-        p.setPayloadSize(sizeof(intigerToSend) + sizeof(extraData));
-        std::memcpy(p.payload(), &intigerToSend, sizeof(intigerToSend));
-        std::memcpy(p.payload() + sizeof(intigerToSend), &extraData, sizeof(extraData));
-
-        con.send(p);
-    }
-
-public:
-private:
-    std::string uri_;
-};
-
-int main()
+    printToc();
+}
+TocInfo Toc::getTocInfo()
 {
-    std::vector<std::string> uris({"usb://0"});
-    std::cout << "connecting to crazyflie via usb 0" << std::endl;
+    //ask for the toc info
+    _con.sendInt(PARAM_PORT, TOC_CHANNEL, CMD_TOC_INFO_V2);
+    bitcraze::crazyflieLinkCpp::Packet p_recv = _con.recv(0);
+    return TocInfo(p_recv);
+}
+TocItem Toc::getItemFromToc(uint16_t id)
+{
+    //ask for a param with the given id
+    _con.sendInt(PARAM_PORT, TOC_CHANNEL, CMD_TOC_ITEM_V2, id);
+    bitcraze::crazyflieLinkCpp::Packet p_recv = _con.recv(0);
+    return TocItem(p_recv);
+}
 
-    std::vector<Benchmark> benchmarks;
-    benchmarks.reserve(uris.size());
-    std::vector<std::thread> threads;
-    threads.reserve(uris.size());
+std::vector<TocItem> Toc::getToc()
+{
+    std::vector<TocItem> tocItems;
 
-    for (const auto &uri : uris)
+    uint16_t num_of_elements = getTocInfo()._numberOfElements;
+    for (uint16_t i = 0; i < num_of_elements; i++)
     {
-        benchmarks.emplace_back(Benchmark(uri));
-        threads.emplace_back(std::thread(&Benchmark::run, &benchmarks.back()));
+        tocItems.push_back(getItemFromToc(i));
     }
-
-    for (auto &t : threads)
+    return tocItems;
+}
+void Toc::printToc()
+{
+    auto tocItems = getToc();
+    for (TocItem tocItem : tocItems)
     {
-        t.join();
+        // tocItem
+        std::cout << tocItem._paramId << ": " << (int)tocItem._paramType << "  " << tocItem._groupName << "." << tocItem._paramName << std::endl;
     }
-
-    return 0;
 }
--- src/Toc.h
+++ src/Toc.h
@@ -1,13 +1,9 @@
 #include <iostream>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <chrono>
 #include <sstream>
-#include <queue>
 #include <vector>
+#include <string>
 
-#include "crazyflieLinkCpp/Connection.h"
+#include "ConnectionPlus.h"
 
 #define PARAM_PORT 2
 #define TOC_CHANNEL 0
@@ -17,7 +13,6 @@
 #define CMD_TOC_ITEM_V2 2 // version 2: up to 16k entries
 #define CMD_TOC_INFO_V2 3 // version 2: up to 16k entries
 
-using namespace bitcraze::crazyflieLinkCpp;
 
 struct TocItem
 {
@@ -27,24 +22,8 @@ struct TocItem
     uint8_t _paramType;
     uint16_t _paramId;
 
-    TocItem(Packet &p_recv)
-    {
-        _cmdNum = p_recv.payload()[0];
-        _paramId = 0;
-        memcpy(&_paramId, &p_recv.payload()[1], sizeof(_paramId));
-        _paramType = p_recv.payload()[3];
-        _groupName = (char *)(&p_recv.payload()[4]);
-        _paramName = (char *)(&p_recv.payload()[4] + _groupName.length() + 1);
-    }
-    friend std::ostream &operator<<(std::ostream &out, const TocItem &tocItem)
-    {
-        out << "cmdNum: " << (int)tocItem._cmdNum << std::endl;
-        out << "paramId: " << (int)tocItem._paramId << std::endl;
-        out << "paramType: " << (int)tocItem._paramType << std::endl;
-        out << "groupName: " << tocItem._groupName << std::endl;
-        out << "paramName: " << tocItem._paramName << std::endl;
-        return out;
-    }
+    TocItem(bitcraze::crazyflieLinkCpp::Packet &p_recv);
+    friend std::ostream &operator<<(std::ostream &out, const TocItem &tocItem);
 };
 
 struct TocInfo
@@ -52,107 +31,23 @@ struct TocInfo
     uint16_t _numberOfElements;
     uint32_t _crc;
 
-    TocInfo(Packet& p_recv)
-    {
-        memcpy(&_numberOfElements, &p_recv.payload()[1], sizeof(_numberOfElements));
-        memcpy(&_crc, &p_recv.payload()[3], sizeof(_crc));
-    }
+    TocInfo(bitcraze::crazyflieLinkCpp::Packet& p_recv);
 
-    friend std::ostream &operator<<(std::ostream &out, const TocInfo &tocInfo)
-    {
-        out << "numberOfElements: " << (int)tocInfo._numberOfElements << std::endl;
-        out << "crc: " << (int)tocInfo._crc << std::endl;
-        return out;
-    }
+    friend std::ostream &operator<<(std::ostream &out, const TocInfo &tocInfo);
 };
 
 class Toc
 {
 public:
-    Toc(const std::string &uri)
-        : _con(uri)
-    {
-    }
+    Toc(const std::string &uri);
 
-    void run()
-    {
+    void run();
+    TocInfo getTocInfo();
+    TocItem getItemFromToc(uint16_t id);
 
-        // empty receiver queue
-        while (true)
-        {
-            Packet p = _con.recv(100);
-            if (!p)
-            {
-                break;
-            }
-        }
-        
-        printToc(_con);
-      
-        
-    }
-    TocInfo getTocInfo()
-    {
-        //ask for the toc info
-        sendInt(_con, PARAM_PORT, TOC_CHANNEL, CMD_TOC_INFO_V2);
-        Packet p_recv = _con.recv(0);
-        return TocInfo(p_recv);
-    }
-    TocItem getItemFromToc(uint16_t id)
-    {
-        //ask for a param with the given id
-        sendInt(_con, PARAM_PORT, TOC_CHANNEL, CMD_TOC_ITEM_V2, id);
-        Packet p_recv = _con.recv(0);
-        return TocItem (p_recv);
-    }
-
-    std::vector<TocItem> getToc()
-    {
-        std::vector<TocItem> tocItems;
-
-        uint16_t num_of_elements = getTocInfo()._numberOfElements;
-        for (uint16_t i = 0; i < num_of_elements; i++)
-        {
-            tocItems.push_back(getItemFromToc(i));
-        }
-        return tocItems;
-    }
-    void printToc()
-    {
-        auto tocItems = getToc();
-        for(TocItem tocItem : tocItems)
-        {
-            // tocItem
-            std::cout << tocItem._paramId << ": " << (int)tocItem._paramType << "  " << tocItem._groupName << "." << tocItem._paramName << std::endl;
-        }
-    }
-
-    
+    std::vector<TocItem> getToc();
+    void printToc();
 
 private:
-    Connection _con;
+    ConnectionPlus _con;
 };
-
-int main()
-{
-    std::vector<std::string> uris({"usb://0"});
-    std::cout << "connecting to crazyflie via usb 0" << std::endl;
-
-    std::vector<Benchmark> benchmarks;
-    benchmarks.reserve(uris.size());
-    std::vector<std::thread> threads;
-    threads.reserve(uris.size());
-
-    for (const auto &uri : uris)
-    {
-        benchmarks.emplace_back(Benchmark(uri));
-        threads.emplace_back(std::thread(&Benchmark::run, &benchmarks.back()));
-    }
-
-    for (auto &t : threads)
-    {
-        t.join();
-    }
-
-    return 0;
-}
