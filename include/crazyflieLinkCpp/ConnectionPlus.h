73be3e15241ce4612bf6c6d5c7c6d1495b8812ca
trakevital <avital.morgenstern@gmail.com>
2021-04-29 11:27:48 +0300

    Added a general sendObject function which sends an object of any type

--- include/crazyflieLinkCpp/ConnectionPlus.h
+++ include/crazyflieLinkCpp/ConnectionPlus.h
@@ -10,7 +10,7 @@
 #include <vector>
 #include "Connection.h"
 
-
+#define PAYLOAD_MAX_SIZE CRTP_MAXSIZE-2
 
 class ConnectionPlus : public bitcraze::crazyflieLinkCpp::Connection
 {
@@ -27,4 +27,18 @@ public:
     
     void sendInt(uint8_t intigerToSend, int port, int channel);
     void sendInt(uint8_t intigerToSend, uint16_t extraData, int port, int channel);
+
+    template <typename IntType>
+    void sendObject(const IntType& objectToSend)
+    {
+        std::cout << (int)objectToSend << std::endl;
+
+        if(sizeof(IntType) > PAYLOAD_MAX_SIZE)
+            throw std::runtime_error("Object to large to send as a backage. Size: " + sizeof(objectToSend));
+        _packet.setPayloadSize(sizeof(objectToSend));
+        std::memcpy(_packet.payload(), &objectToSend, sizeof(objectToSend));
+
+        this->send(_packet);
+    }
+
 };
