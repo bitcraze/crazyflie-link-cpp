520761f39af3ba122cae60c8c6bf36692ccae192
trakevital <avital.morgenstern@gmail.com>
2021-05-04 15:51:38 +0300

    uploaded

---
+++ build/CMakeFiles/cflinkcpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cflinkcpp.dir/src/python_bindings.cpp.o"
+  "cflinkcpp.cpython-36m-x86_64-linux-gnu.so"
+  "cflinkcpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cflinkcpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
