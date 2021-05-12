51d204d8341c4523dfbba3a30b100d3a484dc9a9
Jonas Danielsson <jonas@bitcraze.io>
2021-05-12 10:55:24 +0200

    CrazyRadioThread: unify safelink and non-safelink

--- src/CrazyradioThread.cpp
+++ src/CrazyradioThread.cpp
@@ -180,78 +180,53 @@ void CrazyradioThread::run()
                     if (ack) {
                         con->safelinkInitialized_ = true;
                     }
-                } else {
-                    // send actual packet via safelink
-
-
-                    Packet p(ping, sizeof(ping));
-
-                    if (con->retry_) {
-                        p = con->retry_;
-                    } else {
-                        const std::lock_guard<std::mutex> lock(con->queue_send_mutex_);
-                        if (!con->queue_send_.empty())
-                        {
-                            p = con->queue_send_.top();
-                            con->queue_send_.pop();
-                        } else if (!con->useAutoPing_)
-                        {
-                            continue;
-                        }
-                    }
-
-                    p.setSafelink(con->safelinkUp_ << 1 | con->safelinkDown_);
-                    ack = radio.sendPacket(p.raw(), p.size());
-                    ++con->statistics_.sent_count;
-
-                    if (ack)
-                    {
-                        con->safelinkUp_ = !con->safelinkUp_;
-                        if (con->retry_) {
-                            con->retry_ = Packet();
-                        }
-                    } else {
-                        con->retry_ = p;
-                    }
-
-                    if (ack && ack.size() > 0 && (ack.data()[0] & 0x04) == (con->safelinkDown_ << 2)) {
-                        con->safelinkDown_ = !con->safelinkDown_;
-                    } else {
-                        // reset ack to be invalid since safelink does not match
-                        ack = Crazyradio::Ack();
-                    }
-
                 }
+            }
+
+            Packet p(ping, sizeof(ping));
+
+            if (con->retry_) {
+                p = con->retry_;
             } else {
-                // no safelink
-                Packet p;
+                const std::lock_guard<std::mutex> lock(con->queue_send_mutex_);
+                if (!con->queue_send_.empty())
+                {
+                    p = con->queue_send_.top();
+                    con->queue_send_.pop();
+                } else if (!con->useAutoPing_)
+                {
+                    continue;
+                }
+            }
 
-                if (con->retry_)
+            if (con->useSafelink_)
+            {
+                p.setSafelink(con->safelinkUp_ << 1 | con->safelinkDown_);
+            }
+
+            ack = radio.sendPacket(p.raw(), p.size());
+            ++con->statistics_.sent_count;
+            if (ack)
+            {
+                if (con->useSafelink_)
                 {
-                    p = con->retry_;
-                }
-                else if (!con->queue_send_.empty())
-                {
-                    { // guard the send queue with this block
-                        const std::lock_guard<std::mutex> lock(con->queue_send_mutex_);
-                        p = con->queue_send_.top();
-                        con->queue_send_.pop();
-                    }
-                }
-                else if (con->useAutoPing_)
-                {
-                    p = Packet(ping, sizeof(ping));
+                    con->safelinkUp_ = !con->safelinkUp_;
                 }
 
-                ack = radio.sendPacket(p.raw(), p.size());
-                ++con->statistics_.sent_count;
-                if (ack)
-                {
+                if (con->retry_) {
                     con->retry_ = Packet();
                 }
-                else
-                {
-                    con->retry_ = p;
+            } else {
+                con->retry_ = p;
+            }
+
+            if (con->useSafelink_)
+            {
+                if (ack && ack.size() > 0 && (ack.data()[0] & 0x04) == (con->safelinkDown_ << 2)) {
+                    con->safelinkDown_ = !con->safelinkDown_;
+                } else {
+                    // reset ack to be invalid since safelink does not match
+                    ack = Crazyradio::Ack();
                 }
             }
 
