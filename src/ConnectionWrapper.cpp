041599e289d457ed1e0a71fc9f145c42163a7345
nitayke <nitayke@gmail.com>
2021-05-18 14:03:11 +0300

    recv filtered data clean

--- src/ConnectionWrapper.cpp
+++ src/ConnectionWrapper.cpp
@@ -26,6 +26,7 @@ void ConnectionWrapper::sendData(uint32_t data1, size_t data1_len, uint32_t data
 
     if (IS_BIG_ENDIAN)
     {
+        // std::cout << "IS BIG: " << IS_BIG_ENDIAN << std::endl;
         std::reverse_copy((uint8_t*)&data1 + data1_len, (uint8_t*)&data1, _packet.payload());
         if (data2_len != 0)
         {
@@ -34,42 +35,31 @@ void ConnectionWrapper::sendData(uint32_t data1, size_t data1_len, uint32_t data
     }
     else
     {
-        // std::cout << "IS LITTLE: " << !IS_BIG_ENDIAN << std::endl;
-        // std::cout << "data1: " << data1 << "len: " << data1_len << std::endl;
-        // std::cout << "data2: " << data2 << "len: " << data2_len << std::endl;
         std::copy( (uint8_t*)&data1, (uint8_t*)&data1 + data1_len, _packet.payload());
         if (data2_len != 0)
         {
             std::copy( (uint8_t*)&data2, (uint8_t*)&data2 + data2_len, _packet.payload() + data1_len);
         }
     }
-
-    // std::cout << _packet << std::endl;
     _con.send(_packet);
 }
 
 
 bitcraze::crazyflieLinkCpp::Packet ConnectionWrapper::recvFilteredData(int timeout, int port, int channel)
 {
-    while(true)
+    while (true)
     {
         bitcraze::crazyflieLinkCpp::Packet p = _con.recv(timeout);
-        if (p.channel() == channel && p.port() == port)
-        {
+        if (p.channel() == channel && p.port() == port || !p)
             return p;
-        }
-        else if (!p) //if p is invalid return invalid p
-        {
-            return p;
-        }
-        
-        // std::cout << "package " << p << std::endl;
     }
 }
+
 bitcraze::crazyflieLinkCpp::Packet ConnectionWrapper::recvFilteredData(int timeout)
 {
     return this->recvFilteredData(timeout, _packet.port(), _packet.channel());
 }
+
 Connection &ConnectionWrapper::getConnection()
 {
     return _con;
@@ -79,6 +69,7 @@ void ConnectionWrapper::setPort(int port)
 {
     _packet.setPort(port);
 }
+
 void ConnectionWrapper::setChannel(int channel)
 {
     _packet.setChannel(channel);
