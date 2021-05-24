e0b2b335d46bf9c5884f10639cb5a20a92b9292f
nitayke <nitayke@gmail.com>
2021-05-24 11:15:55 +0300

    changes

--- examples/basicParam.cpp
+++ examples/basicParam.cpp
@@ -20,14 +20,20 @@ int main()
         std::string strType = toc.getAccessAndStrType(tocItem._paramType).second;
         std::string strAccessType = toc.accessTypeToStr(toc.getAccessAndStrType(tocItem._paramType).first);
         cout << tocItem._paramId << ": " << strAccessType << ":" << strType << "  " << tocItem._groupName << "." << tocItem._paramName << " val=";
-
-        cout << param.getById(tocItem._paramId) << endl;
+        if(strType.find("int") != std::string::npos)
+            cout << param.getUIntById(tocItem._paramId) << endl;
+        else 
+            cout << param.getFloatById(tocItem._paramId) << endl;
     }
-    cout << "numOfElements: " << (int) numOfElements << endl;
-    cout << "------------------" << endl;
+    cout << "------------------\n" << "numOfElements: " << (int) numOfElements << "\n------------------\n\n\n" << endl;
 
-    cout << "Get by name:\n";
-    cout << param.getByName("activeMarker", "mode") << endl;
+    cout << "Get by name:                activeMarker . mode  \n";
+    
+    cout << param.getUIntByName("activeMarker", "mode") << endl;
+
+
+    cout << "Get by name:                usd . logging  \n";
+    cout << param.getUIntByName("usd", "logging") << endl;
 
     return 0;
 }
--- src/Param.cpp
+++ src/Param.cpp
@@ -1,6 +1,6 @@
 #include "Param.h"
 
-Param::Param(bitcraze::crazyflieLinkCpp::Connection &con) : _conWrapper(con), _con(con), toc(con)
+Param::Param(bitcraze::crazyflieLinkCpp::Connection &con) : _conWrapper(con), _con(con), _toc(con)
 {
     _conWrapper.setPort(PARAM_PORT);
     _conWrapper.setChannel(PARAM_READ_CHANNEL);
@@ -8,7 +8,7 @@ Param::Param(bitcraze::crazyflieLinkCpp::Connection &con) : _conWrapper(con), _c
 
 float Param::getFloat(uint16_t paramId)
 {
-    float res;
+    float res = 0;
 
     _conWrapper.sendData(paramId, sizeof(paramId));
     bitcraze::crazyflieLinkCpp::Packet p = _conWrapper.recvFilteredData(0);
@@ -19,24 +19,26 @@ float Param::getFloat(uint16_t paramId)
 
 uint32_t Param::getUInt(uint16_t paramId)
 {
-    uint32_t res;
+    uint32_t res = 0;
 
     _conWrapper.sendData(paramId, sizeof(paramId));
     bitcraze::crazyflieLinkCpp::Packet p = _conWrapper.recvFilteredData(0);
-    std::memcpy(&res, p.payload() + PAYLOAD_VALUE_BEGINING_INDEX, sizeof(res));
+    std::memcpy(&res, p.payload() + PAYLOAD_VALUE_BEGINING_INDEX, p.payloadSize() - PAYLOAD_VALUE_BEGINING_INDEX);
+    // std::cout << p << std::endl;
+    // std::cout << "Size: "<< p.payloadSize() << std::endl;
+    // for(size_t i = 0; i < p.payloadSize(); i++)
+    //     std::cout << (unsigned int) p.payload()[i] << "  ";
 
+    // std::cout << std::endl;
     return res;
 }
 
-float Param::getById(short paramId)
+float Param::getFloatById(uint16_t paramId)
 {
-    auto tocItem = toc.getItemFromToc(paramId);
-    std::string strType = toc.getAccessAndStrType(tocItem._paramType).second;
-    if (strType.find("int") != std::string::npos)
-    {
-        return (float)getUInt(paramId);
-    }
-    else if ("float" == strType)
+    auto tocItem = _toc.getItemFromToc(paramId);
+    std::string strType = _toc.getAccessAndStrType(tocItem._paramType).second;
+
+    if ("float" == strType)
     {
         return getFloat(paramId);
     }
@@ -47,16 +49,50 @@ float Param::getById(short paramId)
     }
 }
 
-float Param::getByName(std::string group, std::string name)
+uint32_t Param::getUIntById(uint16_t paramId)
 {
-    uint16_t numOfElements = toc.getTocInfo()._numberOfElements;
+    auto tocItem = _toc.getItemFromToc(paramId);
+    std::string strType = _toc.getAccessAndStrType(tocItem._paramType).second;
+    if (strType.find("int") != std::string::npos)
+    {
+        return getUInt(paramId);
+    }
+    else
+    {
+        std::cout << "Didn't find anything!" << std::endl;
+        return NOT_FOUND;
+    }
+}
+
+float Param::getFloatByName(std::string group, std::string name)
+{
+    uint16_t numOfElements = _toc.getTocInfo()._numberOfElements;
 
     for (uint16_t i = 0; i < numOfElements; i++)
     {
-        auto tocItem = toc.getItemFromToc(i);
+        auto tocItem = _toc.getItemFromToc(i);
+
         if (tocItem._groupName == group && tocItem._paramName == name)
         {
-            return getById(i);
+
+            return getFloatById(tocItem._paramId);
+        }
+    }
+    std::cout << "Didn't find anything!" << std::endl;
+    return NOT_FOUND;
+}
+
+uint32_t Param::getUIntByName(std::string group, std::string name)
+{
+    uint16_t numOfElements = _toc.getTocInfo()._numberOfElements;
+
+    for (uint16_t i = 0; i < numOfElements; i++)
+    {
+        auto tocItem = _toc.getItemFromToc(i);
+
+        if (tocItem._groupName == group && tocItem._paramName == name)
+        {
+            return getUIntById(tocItem._paramId);
         }
     }
     std::cout << "Didn't find anything!" << std::endl;
@@ -65,8 +101,9 @@ float Param::getByName(std::string group, std::string name)
 
 Toc Param::getToc()
 {
-    return toc;
+    return _toc;
 }
 
 Param::~Param()
-{}
+{
+}
