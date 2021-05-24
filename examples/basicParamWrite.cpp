b5b6c0db73e8c33e123c17e9c551426933174596
trakevital <avital.morgenstern@gmail.com>
2021-05-24 16:15:59 +0300

    added argv, argc implementation to basicParamWrite

--- examples/basicParamWrite.cpp
+++ examples/basicParamWrite.cpp
@@ -6,12 +6,21 @@ using std::cout;
 using std::endl;
 using namespace bitcraze::crazyflieLinkCpp;
 
-int main()
+int main(int argc, char *argv[])
 {
+    if(argc < 5)
+    {
+        std::cout << "<group name> <param name> <new value> <byte size>" << std::endl;
+        return 0;
+    }
     Connection con("usb://0");
     Param param(con);
     Toc toc = param.getToc();
-
+    
+    std::string requestedGroupName = argv[1];
+    std::string requestedParamName = argv[2];
+    unsigned int newValue = std::stoul(argv[3]);
+    size_t newValueSize = std::stoul(argv[4]);
     uint16_t numOfElements = toc.getTocInfo()._numberOfElements;
 
     for (uint16_t i = 0; i < numOfElements; i++)
@@ -21,16 +30,16 @@ int main()
         // std::string strType = toc.getAccessAndStrType(tocItem._paramType).second;
         // std::string strAccessType = toc.accessTypeToStr(toc.getAccessAndStrType(tocItem._paramType).first);
 
-        if(tocItem._groupName == "usd" && tocItem._paramName == "logging")
+        if(tocItem._groupName == requestedGroupName && tocItem._paramName == requestedParamName)
         {
-            cout << param.getUIntByName("usd", "logging") << endl;
+            cout << param.getUIntByName(requestedGroupName, requestedParamName) << endl;
 
-            param.setParam(tocItem._paramId, 1, sizeof(uint8_t));
+            param.setParam(tocItem._paramId, newValue, newValueSize);
             break;
         }
     }
-    cout << "Get by name:                usd . logging  \n";
-    cout << param.getUIntByName("usd", "logging") << endl;
+    cout << "Get by name:                \n";
+    cout << param.getUIntByName(requestedGroupName, requestedParamName) << endl;
 
     return 0;
 }
