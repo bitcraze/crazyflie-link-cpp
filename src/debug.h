738ca6bd98c58dc303c05a9cd09d604b75bc3182
trakevital <avital.morgenstern@gmail.com>
2021-06-10 09:12:47 +0300

    Added a header file with macros for debugging

---
+++ src/debug.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+
+#define DEBUG_PRINT(var) (std::cout << #var << ": " << var << std::endl)
+#define DEBUG_PRINT_WITH_CAST(var, cast) (std::cout << #var << ": " << (cast)var << std::endl)
+#define DEBUG_PAUSE()  \
+    {                  \
+        int i = 0;     \
+        std::cin >> i; \
+    }
