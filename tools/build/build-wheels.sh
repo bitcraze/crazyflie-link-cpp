8f4b0fbaff60161a17a500cd62c4637d5eb47b8b
Kristoffer Richardsson <kristoffer@bitcraze.io>
2023-03-16 16:11:17 +0100

    Updated python versions and the way it is built for linux

--- tools/build/build-wheels.sh
+++ tools/build/build-wheels.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -ex
 
-DOCKER=docker
+PYVERSION=${1?"Error: expected the python version"}
 
 # Prefer running with podman
+DOCKER=docker
 if which podman
 then
     DOCKER=podman
@@ -13,21 +14,21 @@ fi
 if [ ! -d /opt/python ]
 then
     # We are not in the image, start the image with the build script
-    $DOCKER run --rm -v $(realpath $(dirname $0)/../..):/io quay.io/pypa/manylinux2014_x86_64 /io/tools/build/build-wheels.sh
+    $DOCKER run --rm -v $(realpath $(dirname $0)/../..):/io quay.io/pypa/manylinux2014_x86_64 /io/tools/build/build-wheels.sh $PYVERSION
 else
     # We are in the image, building!
+    PYVERCL=${PYVERSION//./}
+    # Bins located in paths similar to /opt/python/cp38-cp38/bin
+    PYBIN="/opt/python/cp${PYVERCL}-cp${PYVERCL}/bin"
+    echo "Using python path $PYBIN"
+
     yum install -y libusbx-devel
 
     cd /io
 
-    PREV_PATH=$PATH
-
-    for PYBIN in /opt/python/cp{36,37,38,39}*/bin; do
-        PATH=${PYBIN}:$PREV_PATH
-        rm -rf build/
-        "${PYBIN}/pip" install -U "setuptools>=42" wheel ninja "cmake>=3.12"
-        "${PYBIN}/python" setup.py bdist_wheel
-    done
+    PATH=${PYBIN}:$PATH
+    "${PYBIN}/pip" install -U "setuptools>=42" wheel ninja "cmake>=3.12"
+    "${PYBIN}/python" setup.py bdist_wheel
 
     WHEELS=$(echo dist/*.whl)
 
